07b7ead7085894f085515043638f77611e861f7c
global <global@global.global>
2022-01-11 17:16:51 +0800

    feat(table-config): table-config

--- src/base-ui/table/src/table.vue
+++ src/base-ui/table/src/table.vue
@@ -1,38 +1,46 @@
 <template>
-  <el-table
-    :data="listData"
-    border
-    style="width: 100%"
-    @selection-change="handleSelectionChange"
-  >
-    <el-table-column
-      v-if="showSelectColumn"
-      type="selection"
-      width="60"
-      align="center"
-    ></el-table-column>
-    <el-table-column
-      v-if="showIndexColumn"
-      type="index"
-      label="序号"
-      width="60"
-      align="center"
-    ></el-table-column>
-    <template v-for="propItem in propsList" :key="propItem.prop">
+  <div class="cx-table">
+    <div class="cx-table-header">
+      <slot name="header"></slot>
+    </div>
+    <el-table
+      :data="listData"
+      border
+      style="width: 100%"
+      @selection-change="handleSelectionChange"
+    >
       <el-table-column
+        v-if="showSelectColumn"
+        type="selection"
+        width="60"
         align="center"
-        :prop="propItem.prop"
-        :label="propItem.label"
-        :min-width="propItem.minWidth"
-      >
-        <template #default="scope">
-          <slot :name="propItem.slotName" :row="scope.row">
-            {{ scope.row[propItem.prop] }}
-          </slot>
-        </template>
-      </el-table-column>
-    </template>
-  </el-table>
+      ></el-table-column>
+      <el-table-column
+        v-if="showIndexColumn"
+        type="index"
+        label="序号"
+        width="60"
+        align="center"
+      ></el-table-column>
+      <template v-for="propItem in propsList" :key="propItem.prop">
+        <el-table-column
+          align="center"
+          :prop="propItem.prop"
+          :label="propItem.label"
+          :min-width="propItem.minWidth"
+        >
+          <template #default="scope">
+            <slot :name="propItem.slotName" :row="scope.row">
+              {{ scope.row[propItem.prop] }}
+            </slot>
+          </template>
+        </el-table-column>
+      </template>
+    </el-table>
+    <div class="cx-table-footer">
+      <slot name="footer"></slot>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -4,27 +4,24 @@
       <xz-form v-bind="searchFormConfig" v-model="formData">
         <template #footer>
           <div class="search-btn">
-            <el-button>
-              <el-icon><refresh /></el-icon>
-              <span>重置</span>
-            </el-button>
-            <el-button type="primary">
-              <el-icon><search /></el-icon>
-              <span>搜索</span>
-            </el-button>
+            <el-button icon="el-icon-refresh">重置</el-button>
+            <el-button type="primary" icon="el-icon-search">搜索</el-button>
           </div>
         </template>
       </xz-form>
       <div class="table">
         <xz-table
           :listData="userList"
-          :propsList="propsList"
-          :showIndexColumn="showIndexColumn"
-          :showSelectColumn="showSelectColumn"
+          v-bind="tableConfig"
           @selectionChange="selectionChange"
         >
           <template #enable="scope">
-            <el-button>{{ scope.row.enable ? '启用' : '禁用' }}</el-button>
+            <el-button
+              size="mini"
+              :type="scope.row.enable ? 'success' : 'danger'"
+            >
+              {{ scope.row.enable ? '启用' : '禁用' }}
+            </el-button>
           </template>
           <template #createAt="scope">
             <span>{{ $filter.formatTime(scope.row.createAt) }}</span>
@@ -32,6 +29,17 @@
           <template #updateAt="scope">
             <span>{{ $filter.formatTime(scope.row.updateAt) }}</span>
           </template>
+          <template #handler>
+            <div class="handle-btns">
+              <el-button size="mini" type="text" icon="el-icon-edit"
+                >编辑</el-button
+              >
+              <el-button size="mini" type="text" icon="el-icon-delete"
+                >删除</el-button
+              >
+            </div>
+          </template>
+          <template #header> this.shit! </template>
         </xz-table>
       </div>
     </div>
@@ -43,15 +51,13 @@ import { computed, defineComponent, ref } from 'vue'
 import XzForm from '@/base-ui/form'
 import XzTable from '@/base-ui/table'
 import { searchFormConfig } from './config/search.config'
-import { Refresh, Search } from '@element-plus/icons'
+import { tableConfig } from './config/table.config'
 import { useStore } from '@/store'
 
 export default defineComponent({
   name: 'user',
   components: {
     XzForm,
-    Refresh,
-    Search,
     XzTable
   },
   setup() {
@@ -72,38 +78,6 @@ export default defineComponent({
     })
     const userList = computed(() => store.state.systemModule.userList)
 
-    const propsList = [
-      { prop: 'name', label: '用户名', minWidth: '100', slotName: 'name' },
-      {
-        prop: 'realname',
-        label: '真实姓名',
-        minWidth: '100',
-        slotName: 'realname'
-      },
-      {
-        prop: 'cellphone',
-        label: '电话号码',
-        minWidth: '100',
-        slotName: 'cellphone'
-      },
-      { prop: 'enable', label: '状态', minWidth: '100', slotName: 'enable' },
-      {
-        prop: 'createAt',
-        label: '创建时间',
-        minWidth: '250',
-        slotName: 'createAt'
-      },
-      {
-        prop: 'updateAt',
-        label: '更新时间',
-        minWidth: '250',
-        slotName: 'updateAt'
-      }
-    ]
-
-    const showIndexColumn = true
-    const showSelectColumn = true
-
     const selectionChange = (value: any) => {
       console.log(value)
     }
@@ -112,9 +86,7 @@ export default defineComponent({
       formData,
       searchFormConfig,
       userList,
-      propsList,
-      showIndexColumn,
-      showSelectColumn,
+      tableConfig,
       selectionChange
     }
   }
